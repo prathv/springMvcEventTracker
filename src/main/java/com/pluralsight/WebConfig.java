4c9204d7bf7f09a2e863aa0080c5416bd58b6f6c
prathv <prathveer.r@gmail.com>
2018-01-24 15:10:56 -0800

    Added initial setup and controllers

---
+++ src/main/java/com/pluralsight/WebConfig.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.pluralsight")
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/pdfs/**").addResourceLocations("/WEB-INF/pdf/");
+		registry.addResourceHandler("css/**").addResourceLocations("/WEB-INF/css/");
+	}
+}
